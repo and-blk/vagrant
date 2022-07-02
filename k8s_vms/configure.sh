ed9d6826170ec72ef18fbae832c14911e47b35a8
Andrei Lazarev <and.blkv@gmail.com>
2022-07-02 10:54:16 +0000

    fix k8s issue, add single vm build

--- k8s_vms/configure.sh
+++ k8s_vms/configure.sh
@@ -2,5 +2,12 @@
 
 INFO='\e[0;34mINFO:\e[0m'
 
-echo -e "${INFO} Add user keys"
+echo -e "${INFO} fix containerd configuration file"
 
+cat > /etc/containerd/config.toml <<EOF
+[plugins."io.containerd.grpc.v1.cri"]
+  systemd_cgroup = true
+EOF
+
+echo -e "${INFO} restart containerd daemon"
+systemctl restart containerd
