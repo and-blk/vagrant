0746b7c5793311745d947199a306cb869069fffc
Andrei Lazarev <and.blkv@gmail.com>
2022-07-12 18:56:27 +0000

    change subnet

--- common_scripts/dns_config.sh
+++ common_scripts/dns_config.sh
@@ -3,7 +3,7 @@
 INFO='\e[0;34mINFO:\e[0m'
 
 echo -e "${INFO} config DNS source"
-sudo sed -i 's/addresses: .*/addresses: [192.168.0.200]/g' /etc/netplan/01-netcfg.yaml
+sudo sed -i 's/addresses: .*/addresses: [192.168.166.200]/g' /etc/netplan/01-netcfg.yaml
 
 echo -e "${INFO} netplan apply"
 sudo netplan apply
