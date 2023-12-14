256a9d358a81035a945fd4e7d5a0cf2db090396d
Andrei Lazarev <and.blkv@gmail.com>
2023-12-14 22:27:56 +0300

    fix kubespray

--- k8s-cluster-kubespray/post-config.sh
+++ k8s-cluster-kubespray/post-config.sh
@@ -7,5 +7,14 @@ export  REMOTE_USER=automation
 
 
 echo "Run kubespray ansible playbook"
-cd ${PROJECT_PATH}/kubespray
-ansible-playbook -i inventory/mycluster/inventory.ini  --become --become-user=root --user ${REMOTE_USER} --private-key ../../temp/sshkey cluster.yml
+docker run --rm -it --mount type=bind,source="$(pwd)/k8s-cluster-kubespray",dst=/app \
+  --mount type=bind,source="$(pwd)"/temp,dst=/temp \
+  quay.io/kubespray/kubespray:v2.23.1 bash -c '
+    ls -la /app/
+    ansible-playbook -i /app/kubespray/inventory/mycluster/inventory.ini  --become --become-user=root --user automation --private-key /temp/sshkey /app/kubespray/cluster.yml
+
+
+  '
+# cd ${PROJECT_PATH}/kubespray
+
+# ansible-playbook -i inventory/mycluster/inventory.ini  --become --become-user=root --user ${REMOTE_USER} --private-key ../../temp/sshkey cluster.yml
